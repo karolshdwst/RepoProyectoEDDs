98cf519469304e0eb3b7f8646d615a72ebdf6b09
Avrgael250 <maskallergy@gmail.com>
2025-10-29 18:50:49 -0600

    Cambio ejemplo
    Cambio de comentario

--- ProyectoEDDs/src/main/java/org/example/Menu.java
+++ ProyectoEDDs/src/main/java/org/example/Menu.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Menu {
     public static void main(String[] args){
-        //en este archivo se va a crear el menú principal
+        // Muy buenas chavales soy trebor
     }
 }
